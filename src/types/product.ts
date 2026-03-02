1035c0324c7d3cd7a422c705ae412a108f94dec0
Gonzalo Ortiz <98592982+GonzaaOrtiz16@users.noreply.github.com>
2026-03-02 07:08:14 -0300

    Update product.ts

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,13 +1,15 @@
 export interface Product {
   id: string;
   name: string;
+  description: string;
   price: number;
-  image_urls: string[];
-  description?: string;
-  category?: string;
+  image_urls: string[]; // Usamos array para el carrusel
+  category: string;
+  brand?: string;
   created_at?: string;
 }
 
+// ESTA INTERFAZ ES OBLIGATORIA PARA EL CARRITO
 export interface CartItem extends Product {
   quantity: number;
 }
